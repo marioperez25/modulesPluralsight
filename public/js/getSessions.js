95d5f3d4f1fe39b07a9b26c07788ccacad242b34
marioperez25 <mariofph@gmail.com>
2020-09-15 11:06:05 -0600

    moved functions to modules

---
+++ public/js/getSessions.js
@@ -0,0 +1,16 @@
+const sessionURL = 'sessions.json';
+
+let sessionList = [];
+
+export default function getSessions(){
+    return new Promise(function(resolve, reject){
+      var oReq = new XMLHttpRequest();
+      oReq.onload = function (e) {
+          sessionList = e.target.response;
+        resolve(sessionList);
+      };
+      oReq.open('GET', sessionURL, true);
+      oReq.responseType = 'json';
+      oReq.send();
+    })
+  }
